2008100d3604f9059d90ff48ff61b7d6119c977b
RavenBP <ravenbpowless@gmail.com>
2020-11-10 18:56:39 -0500

    Added ability for enemies to change direction when reaching wall

--- GAME2014-F2020-Lab6-main/Assets/_Scripts/Platformer/OpossumBehaviour.cs
+++ GAME2014-F2020-Lab6-main/Assets/_Scripts/Platformer/OpossumBehaviour.cs
@@ -8,8 +8,10 @@ public class OpossumBehaviour : MonoBehaviour
     public SpriteRenderer spriteRenderer;
     public Rigidbody2D rigidbody2D;
     public Transform lookAheadPoint;
+    public Transform lookInFrontPoint;
     public bool isGroundAhead;
-    public LayerMask layerMask;
+    public LayerMask collisionGroundLayer;
+    public LayerMask collisionWallLayer;
     public float direction;
 
     // Start is called before the first frame update
@@ -24,13 +26,24 @@ public class OpossumBehaviour : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        _LookInFront();
         _LookAhead();
         _Move();
     }
 
+    private void _LookInFront()
+    {
+        if (Physics2D.Linecast(transform.position, lookInFrontPoint.position, collisionWallLayer) == true)
+        {
+            _FlipX();
+        }
+
+        Debug.DrawLine(transform.position, lookInFrontPoint.position, Color.red);
+    }
+
     private void _LookAhead()
     {
-        isGroundAhead = (Physics2D.Linecast(transform.position, lookAheadPoint.position, layerMask));
+        isGroundAhead = (Physics2D.Linecast(transform.position, lookAheadPoint.position, collisionGroundLayer));
 
         Debug.DrawLine(transform.position, lookAheadPoint.position, Color.green);
     }
@@ -43,10 +56,15 @@ public class OpossumBehaviour : MonoBehaviour
         }
         else
         {
-            transform.localScale = new Vector3(transform.localScale.x * -1.0f, transform.localScale.y, transform.localScale.z);
-            direction *= -1.0f;
+            _FlipX();
         }
 
         rigidbody2D.velocity *= 0.9f;
+    }
+
+    private void _FlipX()
+    {
+        transform.localScale = new Vector3(transform.localScale.x * -1.0f, transform.localScale.y, transform.localScale.z);
+        direction *= -1.0f;
     }
 }
